d58ab3bbda846d4b5b612d9eb405b210e9b0fca2
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-12 22:43:56 +0700

    Create make a transfer

---
+++ Test Suites/Test Suite Only/Make a Transfer.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Make a Transfer</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7094ef98-aac3-4adf-8392-f6900d7f98d8</testSuiteGuid>
+   <testCaseLink>
+      <guid>780f117b-c9c9-4b50-9c24-71c3503a1e8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>31e22df1-4e0e-434a-8b83-d1b80653e880</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9ba1826c-4562-4e19-ac0e-6f8678d9c141</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>259fbc1e-f16f-4ec8-8212-d9df40b7aaff</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0ce62a7-1a03-4728-857a-4f8953e424f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transfer Page/Make a Transfer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d99c6773-9e28-44b5-8ba5-213f72cf4a74</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
