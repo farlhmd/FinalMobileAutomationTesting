6f5ebfbe19ec7fc7afc3b858de76c5281cb45535
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-12 21:38:37 +0700

    Create DDT - Add new Account.ts

---
+++ Test Suites/Test Suite Only/DDT - Add new Account.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT - Add new Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b8f37879-08ea-43c4-8bb5-78f81cb318ed</testSuiteGuid>
+   <testCaseLink>
+      <guid>c9bb081c-a780-4de6-bfcc-47783225f1ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>31e22df1-4e0e-434a-8b83-d1b80653e880</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9ba1826c-4562-4e19-ac0e-6f8678d9c141</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>259fbc1e-f16f-4ec8-8212-d9df40b7aaff</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>681e8fdc-5768-4671-8cbe-0c7fef5e46ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Accounts Page/Add new Account</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4780d6e8-d779-48c3-a00a-47890ab1dd57</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Account Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>4780d6e8-d779-48c3-a00a-47890ab1dd57</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>accountName</value>
+         <variableId>3fecac8d-19d6-4de4-81d0-8ae501947513</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4780d6e8-d779-48c3-a00a-47890ab1dd57</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>initialBalance</value>
+         <variableId>948a9b4e-4020-45ad-989d-bd29fb4b7b71</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
